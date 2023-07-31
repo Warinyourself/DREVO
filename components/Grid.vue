eca9693406bf31b6059166604bc0351ec4435cf7
Warinyourself <warinyourself@gmail.com>
2023-07-31 22:21:44 +0300

    ☦️

--- components/Grid.vue
+++ components/Grid.vue
@@ -2,14 +2,14 @@
     <div class="wrapper">
         <div class="grid">
             <NuxtLink
-                v-for="(item, index) in itemList"
-                :key="item + index"
-                :to="`/${item}`"
-                class="item"
+                v-for="item in itemList"
+                :key="item.name"
+                :to="`/${item.name}`"
+                :class="`item ${item.class}`"
             >
                 <Icon
-                    :name="item"
-                    :style="{ 'view-transition-name': `icon-${item}` }"
+                    :name="item.name"
+                    :style="{ 'view-transition-name': `icon-${item.name}` }"
                 />
             </NuxtLink>
         </div>
@@ -17,17 +17,44 @@
 </template>
 
 <script lang="ts" setup>
-const itemList = ref(['doll', 'doll1', 'main', 'union'])
+const itemList = ref([
+    {
+        name: 'doll',
+        class: 'medium'
+    },
+    {
+        name: 'doll1',
+    },
+    {
+        name: 'main',
+    },
+    {
+        name: 'union',
+        class: 'medium'
+    }
+])
+
 </script>
 
 <style lang="stylus">
 .grid
+    --amount 4
+    --gap calc(var(--item-size) / 16)
     display grid
-    gap calc(var(--item-size) / 16)
+    gap var(--gap)
     grid-auto-rows var(--item-size)
-    grid-template-columns repeat(3, var(--min-item-size))
-    & > *:first-of-type
-        --item-size: 40vmin
-        grid-column 1 / 3
-        grid-row 1 / 3
+    width calc(var(--min-item-size) * var(--amount) + var(--gap) * (var(--amount) - 1))
+    grid-template-columns repeat(auto-fill, var(--min-item-size))
+    grid-gap 8px
+    grid-auto-rows var(--min-item-size)
+    grid-auto-flow dense
+    & > *
+        --size 1
+        --item-size calc(var(--min-item-size) * var(--size))
+        grid-column-end span var(--size)
+        grid-row-end span var(--size)
+    & .medium
+        --size 2
+    & .big
+        --size 3
 </style>
